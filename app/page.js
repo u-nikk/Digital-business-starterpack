67882183e690b477cd07b6e06695697aea14e9e2
u-nikk <abhinav.te21@bmsce.ac.in>
2023-12-29 01:14:59 +0530

    Hero section start

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 
 import Navbar from '@/src/utils/components/Navbar/Navbar';
 import './page.css'
+import Hero from '@/src/utils/components/Hero/Hero';
 export default function Home() {
 
   return (
     <div className="app">
       <Navbar/>
+      <Hero/>
     </div>
   );
 }
---
+++ src/utils/components/Hero/Hero.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Hero.css'
+const Hero = () => {
+    return (
+        <div>
+            <div className="h-wrapper">
+                <div className="container">
+                    <div className="h-container">
+                        Hero Section
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Hero
