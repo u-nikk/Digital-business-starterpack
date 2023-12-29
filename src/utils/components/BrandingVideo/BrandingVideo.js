12ce880d9246c5a538883d7560965a8332f56ad6
u-nikk <abhinav.te21@bmsce.ac.in>
2023-12-30 02:17:57 +0530

    Completed video

--- src/utils/components/BrandingVideo/BrandingVideo.js
+++ src/utils/components/BrandingVideo/BrandingVideo.js
@@ -1,11 +1,28 @@
-import React from 'react'
+"use client"
+import React, { useRef } from 'react'
 import  './BrandingVideo.css'
+import { motion,useScroll, useTransform } from 'framer-motion'
 const BrandingVideo = () => {
+    const ref = useRef(null)
+
+    const{scrollYProgress} = useScroll({
+        target:ref,
+        offset:["start end","end end"]
+    })
+
+    const scale = useTransform(scrollYProgress,[0,1],[.6,1])
   return (
     <div className="bv-container">
         <div className="container">
-            <motion.video className='bv-video'
-            loop muted autoPlay content=''
+
+            <motion.video
+            ref = {ref}
+            className="bv-video"
+            loop
+             muted 
+             autoPlay
+              content=""
+              style={{scale}}
             src="/video.mp4" typeof='video/mp4'></motion.video>
         </div>
     </div>
