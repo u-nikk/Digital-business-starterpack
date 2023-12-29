7976b5f27feec433f5f949a2f1bb6051bf583fed
u-nikk <abhinav.te21@bmsce.ac.in>
2023-12-30 00:28:50 +0530

    Made Hero and video

--- src/utils/components/Hero/Hero.js
+++ src/utils/components/Hero/Hero.js
@@ -6,7 +6,23 @@ const Hero = () => {
             <div className="h-wrapper">
                 <div className="container">
                     <div className="h-container">
-                        Hero Section
+                        {/*left side*/}
+                        <div className="h-left">
+                            leftside
+                        </div>
+                        {/*right side*/}
+                        <div className="right-side">
+                        <div className="h-title">
+                            <span>Redefine How</span>
+                            <span>You Grow Your</span>
+                            <span>Bussiness</span>
+                        </div>
+                        <div className="h-des">
+                        At Contentic Media, we specialize in curating compelling stories that elevate your brand through strategic influencer marketing. Our platform is the nexus between brands and influential social media personalities, creating a powerful synergy to captivate audiences and drive meaningful action.
+
+                        </div>
+                        <span>Email Box</span>
+                        </div>
                     </div>
                 </div>
             </div>
